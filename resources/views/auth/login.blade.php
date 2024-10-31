0ae51faad5df41d0ca36b6680ba6a9fe99bd37b8
Elder Chiroque <elderaldairc64@gmail.com>
2024-10-31 11:39:00 -0500

    Modificacion del diseño login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
-<html lang="resources/views/welcome.blade.php">
+<html lang="es">
 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>.:. Iniciar Sesión .:.</title>
-  <title>{{ config('app.name') }}</title>
   @include('includes.styles')
 </head>
 
 <body class="hold-transition login-page">
-  <div class="login-box"> <!-- /.login-logo -->
+  <div class="login-box">
     <div class="card card-outline card-danger">
-      <div class="card-header text-center">
-        <a href="" class="h1"><b>Mober</b>'<b>s</b></a>
-      </div>
+      <a href="#" class="brand-link text-center">
+        <img src="{{ asset('dist/img/BSNTS.png') }}" alt="Logo de AdminLTE" class="brand-image img-circle elevation-3" style="opacity: .8; max-width: 100px;">
+        <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
+      </a>
+      
       <div class="card-body">
-        <p class="login-box-msg">Iniciar sesión</p>
         <form action="{{ route('login.action') }}" method="POST" class="user">
           @csrf
           @if ($errors->any())
@@ -33,7 +33,7 @@
               <label class="text-danger">*</label>
               <label class="form-label">Usuario</label>
               <div class="input-group">
-                <input name="usuario" type="text" class="form-control form-control-user @error('usuario')is-invalid @enderror" id="exampleInputName" placeholder="Nombre de Usuario">
+                <input name="usuario" type="text" class="form-control form-control-user @error('usuario') is-invalid @enderror" id="exampleInputName" placeholder="Nombre de Usuario">
                 <div class="input-group-append">
                   <div class="input-group-text">
                     <span class="fas fa-user"></span>
@@ -47,7 +47,7 @@
               <label class="text-danger">*</label>
               <label class="form-label">Contraseña</label>
               <div class="input-group">
-                <input name="password" type="password" class="form-control form-control-user @error('password')is-invalid @enderror" id="exampleInputPassword" placeholder="Contraseña">
+                <input name="password" type="password" class="form-control form-control-user @error('password') is-invalid @enderror" id="exampleInputPassword" placeholder="Contraseña">
                 <div class="input-group-append">
                   <div class="input-group-text">
                     <span class="fas fa-lock"></span>
